caf522c472bc63aabadfda19844a81a554cf16c4
Chad Powell <chad.powell@student.awesomeinc.org>
2022-02-15 19:01:02 +0000

    update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+// CREATE PAGE HEADING ELEMENT
 const heading = document.createElement('h1');
 heading.innerText = 'bouncing balls';
 document.body.appendChild(heading);
+
+// CREATE PAGE CANVAS ELEMENT
+const canvas = document.createElement('canvas');
+document.body.appendChild(canvas);
